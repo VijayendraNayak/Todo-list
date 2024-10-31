5febe5d9760b70f48136376f4353e5dd80b19b26
VijayenraNayak <vijayendranayak19@gmail.com>
2024-10-31 23:45:40 +0530

    Add section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-
 export default function Home() {
   return (
-    <div>
-      hello
-    </div>
+    <main className="h-screen pt-16">
+      <div className="h-[calc(100vh-64px)] flex flex-col max-w-4xl mx-auto p-8">
+        <p className="text-5xl font-semibold mb-8">
+          Daily To Do List
+        </p>
+        <div className="relative max-w-2xl ">
+          <input
+            type="text"
+            placeholder="Add new list item"
+            className="w-full px-4 py-4 rounded-md border border-gray-200 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 placeholder-gray-400 text-gray-600 pr-24"
+          />
+          <button 
+            className="absolute right-2 top-1/2 -translate-y-1/2 px-6 py-2.5 bg-blue-500 text-white font-medium text-xl rounded-md hover:bg-blue-600 transition-colors duration-200"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <header className='fixed top-0 left-0 z-50 w-full bg-blue-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto h-16 px-4'>
+        <div className='flex items-center space-x-2'>
+          <div className='flex items-center'>
+            <span className='text-gray-800 font-bold text-3xl'>Todo</span>
+            <span className='text-blue-500 font-bold text-3xl'>.</span>
+            <span className='text-gray-500 font-medium text-xl ml-1.5'>List</span>
+          </div>
+        </div>
+        <nav>
+          <button className='px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300 font-semibold text-base'>
+            Login
+          </button>
+        </nav>
+      </div>
+    </header>
+  )
+}
